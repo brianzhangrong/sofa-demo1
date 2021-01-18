7debaaceaa1e1f9cf516dcbafe2fe7428d075d7c
zhangrong <39478871+brianzhangrong@users.noreply.github.com>
2021-01-18 17:23:15 +0800

    init

---
+++ src/main/java/com/ihomefnt/ark/demo/Demo1Service.java
@@ -0,0 +1,8 @@
+package com.ihomefnt.ark.demo;
+
+public class Demo1Service {
+    public String init(){
+        return "Demo1Service init";
+    }
+
+}
---
+++ src/main/java/com/ihomefnt/ark/demo/controller/Demo1Controller.java
@@ -0,0 +1,15 @@
+package com.ihomefnt.ark.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Demo1Controller {
+
+    @GetMapping(path="/testResult1")
+    public String testResult(){
+
+        return
+                "demo1 testResult ok";
+    }
+}
